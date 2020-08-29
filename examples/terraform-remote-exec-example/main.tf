90d9becbabd4044e3b9de8fd64860da897da885d
jamesnyika <jnyika@hashicorp.com>
2020-08-28 20:18:21 -0400

    removed file prov

--- examples/terraform-remote-exec-example/main.tf
+++ examples/terraform-remote-exec-example/main.tf
@@ -90,24 +90,14 @@ resource "null_resource" "example_provisioner" {
     agent = true
   }
 
-  // copy our example script to the server
-  provisioner "file" {
-    source      = "files/get-public-ip.sh"
-    destination = "/tmp/get-public-ip.sh"
-  }
 
   // change permissions to executable and pipe its output into a new file
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/get-public-ip.sh",
-      "/tmp/get-public-ip.sh > /tmp/public-ip",
+      "echo 'hello world' > ~/REMOTEOUTPUT.txt",
     ]
   }
 
-  provisioner "local-exec" {
-    # copy the public-ip file back to CWD, which will be tested
-    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${var.ssh_user}@${aws_instance.example_public.public_ip}:/tmp/public-ip public-ip"
-  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
